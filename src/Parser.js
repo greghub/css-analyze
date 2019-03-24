4e1c475a58d706b9a017da3ad29aa953d4ff83d2
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-03-24 15:13:24 +0400

    Refactor and fix linting errors

--- src/Parser.js
+++ src/Parser.js
@@ -1,11 +1,11 @@
 const Parser = {
-    css: ``,
-    parseCSS: function () {
+    css: '',
+    parseCSS: () => {
         const exp = new RegExp(/{[^}]*}/, 'gs');
         const results = this.css.match(exp);
         const properties = [];
 
-        results.forEach(result => {
+        results.forEach((result) => {
             const e = new RegExp(/([a-z-]+):/, 'gi');
             properties.push(result.match(e).map(item => item.replace(':', '')));
         });
